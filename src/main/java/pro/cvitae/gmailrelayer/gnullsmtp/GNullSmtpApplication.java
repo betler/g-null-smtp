6b777652779cebe317577ed3de070c11cdab087d
BeTLeR <17320117+betler@users.noreply.github.com>
2020-04-27 11:47:31 +0200

    Repo creation

---
+++ src/main/java/pro/cvitae/gmailrelayer/gnullsmtp/GNullSmtpApplication.java
@@ -0,0 +1,13 @@
+package pro.cvitae.gmailrelayer.gnullsmtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GNullSmtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GNullSmtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/cvitae/gmailrelayer/gnullsmtp/GNullSmtpApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.cvitae.gmailrelayer.gnullsmtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GNullSmtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
